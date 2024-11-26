c566a91b9d11476f90b17130ffcfca5593f456e9
Phani Kumar Veluturi <veluturi.kumar@vanderlande.com>
2024-11-26 21:20:02 +0530

    shell script is fixed

---
+++ shellscript/kubernets_manage.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+###  K8S Cluster ####
+export k8s_modules01=("overlay" "br_netfilter")
+export KRMODULESK8S='/etc/modules-load.d/k8s_modules.conf'
+export k8s_kernel_nw01=("net.ipv4.ip_forward=1" "net.bridge.bridge-nf-call-iptables=1" "net.bridge.bridge-nf-call-ip6tables=1")
+export SYSCTLK8S99='/etc/sysctl.d/99-kubernetes.conf'
+
+export KUBECTLVER='v1.31'
+export INSTALLURL_DEB="https://pkgs.k8s.io/core:/stable:/${KUBECTLVER}/deb"
+export APTKY_DEB_K8='/etc/apt/keyrings/kubernetes-apt-keyring.gpg'
+export DEBREPO="/etc/apt/sources.list.d/kubernetes.list"
+
+export SYSTMD='sudo systemctl'
+export DISABLE_DAEMON='apparmor'
+export ENABLE_DAEMON='containerd kubelet'
+
+export APTGET='sudo apt-get'
+export REQ_PPK='apt-transport-https ca-certificates curl gpg jq'
+
+export KUBE_PKG='containerd jq kubelet kubeadm kubectl'
+### END K8S Cluster ####
+
+SwapOF(){
+    swap_status=$(swapon --noheadings);	if [ -z "$swap_status" ]; then  echo "Swap memory is OFF."; else sudo swapoff -a; sudo sed -i '/swap/ s/^\(.*\)$/#\1/g' /etc/fstab;(echo "@reboot /sbin/swapoff -a") | sudo crontab - || true; fi
+ }
+SwapOF
+
+InstallREQPKG(){
+	curl -fsSL $INSTALLURL_DEB/Release.key | sudo gpg --dearmor -o $APTKY_DEB_K8
+	echo "deb [signed-by=${APTKY_DEB_K8}] ${INSTALLURL_DEB}/ /" | sudo tee $DEBREPO
+	$APTGET clean all
+	echo "Update packages list"
+	$APTGET update
+	echo "Install containerd"
+	$APTGET -y install $REQ_PPK $KUBE_PKG
+ }
+
+KernelContainerD_Modules(){
+  for LoMD in "${k8s_modules01[@]}"; do
+		if lsmod | grep -q "^${LoMD} "; then
+			echo "The module '${LoMD}' is already loaded."
+		else
+			if ! grep -q "^${LoMD}$" "$KRMODULESK8S"; then
+				echo "${LoMD}" | sudo tee -a "$KRMODULESK8S" > /dev/null
+				echo "Module '${LoMD}' added to $KRMODULESK8S."
+			fi
+			sudo modprobe "${LoMD}"
+			if lsmod | grep -q "^${LoMD} "; then
+				echo "The module '${LoMD}' has been loaded."
+			else
+				echo "Failed to load the module '${LoMD}'."
+			fi
+		fi
+  done
+ }
+
+K8sNW_KERNL_Parameters(){
+  if [ ! -f "$SYSCTLK8S99" ]; then
+		for item1 in "${k8s_kernel_nw01[@]}"; do
+			echo "$item1" | sudo tee -a "$SYSCTLK8S99" > /dev/null
+			sudo sysctl -w "$item1"
+		done
+  fi
+
+  for item1 in "${k8s_kernel_nw01[@]}"; do
+		key=$(echo "$item1" | cut -d'=' -f1)
+		value=$(echo "$item1" | cut -d'=' -f2)
+		current_value=$(sysctl -n "$key" 2>/dev/null)
+
+		if [ "$current_value" != "$value" ]; then
+			sudo sysctl -w "$item1"
+		fi
+
+		if ! grep -Fxq "$item1" "$SYSCTLK8S99"; then
+			echo "$item1" | sudo tee -a "$SYSCTLK8S99" > /dev/null
+			sudo sysctl -w "$item1"
+		fi
+  done
+  sudo sysctl --system &>/dev/null
+ }
+
+containerdCGroup_Driver(){
+	if [ ! -f /etc/containerd/config.toml ]; then
+			sudo mkdir -p /etc/containerd
+			sudo sh -c "containerd config default > /etc/containerd/config.toml"
+			sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+	else
+			SysDGroup=$(cat /etc/containerd/config.toml | grep SystemdCgroup | sed 's/ //g' | sed 's/=//g')
+			if [ "$SysDGroup" == "SystemdCgroupfalse" ]; then
+				 sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+			fi
+	fi
+ }
+
+InstallREQPKG
+KernelContainerD_Modules
+K8sNW_KERNL_Parameters
+containerdCGroup_Driver
+
+$SYSTMD disable $DISABLE_DAEMON; $SYSTMD stop $DISABLE_DAEMON
+echo "Restarting containerd"
+$SYSTMD daemon-reload
+$SYSTMD restart $ENABLE_DAEMON; $SYSTMD enable $ENABLE_DAEMON
+sudo apt-mark hold $KUBE_PKG
+#
+echo "Installing helm packager"
+# sudo curl $HELM_INSTALLURL | bash
+sudo snap install helm --classic
